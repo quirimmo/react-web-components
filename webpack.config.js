c50cf2753f7e902908979a8144ecc77de95fee43
quirimmo <quirimmo@gmail.com>
2018-03-10 01:21:25 +0000

    fixed warnings issues

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,9 @@ const BUILD_DIR = path.resolve(__dirname, 'dist');
 const SRC_DIR = path.resolve(__dirname, 'src');
 
 const config = {
-	entry: {
-		bundle: SRC_DIR + '/index.jsx'
-	},
+	mode: 'development',
+	performance: { hints: false },
+	entry: SRC_DIR + '/index.jsx',
 	devtool: 'inline-source-map',
 	plugins: [
 		new CleanWebpackPlugin([BUILD_DIR]),
@@ -19,7 +19,7 @@ const config = {
 	],
 	output: {
 		path: BUILD_DIR,
-		filename: '[name].js'
+		filename: 'bundle.js'
 	},
 	node: {
 		dns: 'mock',
